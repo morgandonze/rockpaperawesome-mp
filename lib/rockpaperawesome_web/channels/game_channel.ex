ca7fd8335331faef3ef0cc505fbf573a4a123280
Morgan Laco <morganlaco@gmail.com>
2017-10-08 17:22:03 -0400

    Refactor user Presence tracking
    * Add player number to user presence
    * Move presence data definition to user_presence method
    * Rename method to simpler 'game_id'

--- lib/rockpaperawesome_web/channels/game_channel.ex
+++ lib/rockpaperawesome_web/channels/game_channel.ex
@@ -8,28 +8,18 @@ defmodule Rockpaperawesome.GameChannel do
   end
 
   def handle_info({:after_join, game_id}, socket) do
-    {:ok, _} = Presence.track(socket, socket.assigns.user_id, %{
-      user_name: socket.assigns.user_name,
-      online_at: inspect(System.system_time(:seconds)),
-      game_id: game_id,
-    })
-
+    {:ok, _} = Presence.track(socket, socket.assigns.user_id, user_presence(socket, game_id))
     push(socket, "presence_state", Presence.list(socket))
-
     {:noreply, socket}
   end
 
   def handle_in("throw", move, socket) do
     user_id = socket.assigns.user_id
-    game_id = get_game_id_from_presence(user_id, socket)
+    game_id = game_id(socket, user_id)
 
     with {:ok, game} <- GameServer.get_game(game_id),
          game <- Game.make_move(game, user_id, move),
          game <- Game.update_score(game) do
-
-      user_id
-      |> Game.player_num(game)
-      |> track_player_number(socket)
 
       GameServer.update_game(game, game_id)
       broadcast(socket, "throw_complete", game)
@@ -38,16 +28,25 @@ defmodule Rockpaperawesome.GameChannel do
     {:noreply, socket}
   end
 
-  def get_game_id_from_presence(user_id, socket) do
+  def game_id(socket, user_id) do
+    Presence.list(socket)
     %{^user_id => %{metas: metas}} = Presence.list(socket)
+    # Multiple metas happen when a user logs in from multiple clients
+    # I'll just ignore metas other than the first
     [%{game_id: game_id} | _] = metas
     game_id
   end
 
-  def track_player_number(n, socket) do
-    Presence.update(socket, socket.assigns.user_id, %{
-      player_number: n
-    })
-    Presence.list(socket) |> IO.inspect
+  def user_presence(socket, game_id) do
+    %{
+      user_name: socket.assigns.user_name,
+      player_number: player_number(game_id, socket),
+      game_id: game_id
+    }
+  end
+
+  def player_number(game_id, socket) do
+    {:ok, game} = GameServer.get_game(game_id)
+    Game.player_number(game, socket.assigns.user_id)
   end
 end
