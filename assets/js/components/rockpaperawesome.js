cc5970d92dc88bd42f28b6bd9eb60d16d664e281
Morgan Laco <morganlaco@gmail.com>
2017-10-09 02:00:09 -0400

    Start converting front-end to React

---
+++ assets/js/components/rockpaperawesome.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class Rockpaperawesome extends Component {
+  render () {
+    return (
+      <h1>Rockpaperawesome!</h1>
+    )
+  }
+}
+
+export default Rockpaperawesome
