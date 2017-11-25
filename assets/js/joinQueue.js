370f992fab22896f687702ac82a2e6e03febd9bd
Morgan Laco <morganlaco@gmail.com>
2017-11-25 15:22:17 -0500

    Prevent immediate calling of joinQueue on import

--- assets/js/joinQueue.js
+++ assets/js/joinQueue.js
@@ -2,10 +2,12 @@ import queue from './queue'
 import setGame from './setGame'
 
 export default (elem) => {
-  const { userName, parent } = elem.state
-  queue(userName, setGame(parent))
-  let lookingElem = document.getElementById("looking")
-  if (!!lookingElem) {
-    lookingElem.innerHTML = "<h3>Looking for game...</h3>"
+  return function () {
+    const { userName, parent } = elem.state
+    queue(userName, setGame(parent))
+    let lookingElem = document.getElementById("looking")
+    if (!!lookingElem) {
+      lookingElem.innerHTML = "<h3>Looking for game...</h3>"
+    }
   }
 }
