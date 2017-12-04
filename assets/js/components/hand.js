bfd48a3f523b33f9054eb5aeb612cf5ba971b0a0
Morgan Laco <morganlaco@gmail.com>
2017-12-03 23:40:18 -0500

    Correctly orient play area Throw component icons

--- assets/js/components/hand.js
+++ assets/js/components/hand.js
@@ -21,16 +21,35 @@ class Hand extends Component {
 
   render () {
     const { hand, side } = this.state
+    let orientation = this.orientation(hand, side)
     return (
       <FontAwesome
         name={"hand-"+hand+"-o"}
-        className={"fa-3x " + side}
+        className={"fa-3x " + side + orientation}
       />
     )
   }
 
   translateHand = (hand) => {
     return ['rock', 'paper', 'scissors'][hand-1]
+  }
+
+  orientation = (hand, side) => {
+    if (side=='player') {
+      if (hand=='scissors') {
+        return ' rotate-180-flip'
+      } else {
+        return ' fa-rotate-90'
+      }
+    }
+
+    if (side=='opponent') {
+      if (hand=='scissors') {
+        return ' '
+      } else {
+        return ' rotate-270-flip'
+      }
+    }
   }
 }
 
