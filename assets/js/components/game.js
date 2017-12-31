6b3ceb2e247de4de6dec51db0d21d70aff5bb505
Morgan Laco <morganlaco@gmail.com>
2017-12-30 20:42:17 -0500

    Add turn timer
    Players are given a fixed amount of time to make their move each turn. This timer manages the turn time. Next, it will be used to lock the controls and display a message informing the player that time is up.

--- assets/js/components/game.js
+++ assets/js/components/game.js
@@ -2,26 +2,66 @@ import React, { Component } from 'react'
 import Scores from './scores'
 import Throws from './throws'
 import ThrowControls from './throwControls'
+const TURN_DURATION = 4000
 
 class Game extends Component {
   constructor (props) {
     super(props)
+    this.turnActive = false
     this.state = {
       game: props.game,
       player: props.player,
+      moveNum: 0
     }
   }
 
   componentWillReceiveProps(props) {
+    const { moveNum } = this.state
     const { data } = props
-    let moveNum = (data && data.turns && data.turns.length) || 0
+    let newMoveNum = (data && data.turns && data.turns.length) || 0
+
+    if (newMoveNum > moveNum) {
+      this.startTurn()
+      this.setState({
+        moveNum: newMoveNum
+      })
+    } else {
+      this.setState({moveNum: moveNum})
+    }
+
     this.setState({
       game: props.game,
       player: props.player,
       scores: data && data.scores,
-      turns: data && data.turns,
-      moveNum: moveNum
+      turns: data && data.turns
     })
+  }
+
+  componentDidMount() {
+    this.startTurn()
+  }
+
+  componentWillUnmount() {
+    this.timerID && clearInterval(this.timerID)
+  }
+
+  startTurn = () => {
+    console.log('turn started')
+    if (!!this.timerID) clearInterval(this.timerID)
+    this.timerID = setInterval(this.expireTurn, TURN_DURATION)
+    this.turnActive = true
+  }
+
+  expireTurn = () => {
+    console.log('turn expired')
+    this.timerID && clearInterval(this.timerID)
+    this.timerID = null
+    this.turnActive = false
+  }
+
+  moveTime = () => {
+    const { moveStart } = this.state
+    return Date.now() - moveStart
   }
 
   render () {
