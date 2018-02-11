f19d62f9a1a669c736ff39e0dc001ba9189e38e4
Morgan Laco <morganlaco@gmail.com>
2018-02-10 20:46:25 -0500

    Fix prod config

--- config/prod.exs
+++ config/prod.exs
@@ -15,14 +15,14 @@ use Mix.Config
 # which you typically run after static files are built.
 config :rockpaperawesome, RockpaperawesomeWeb.Endpoint,
   load_from_system_env: true,
-  url: [host: "example.com", port: 80],
+  url: [scheme: "https", host: "rockpaperawesome-game.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
 config :hello, Rockpaperawesome.Repo,
   adapter: Ecto.Adapters.Postgres,
-  url: [scheme: "https", host: "rockpaperawesome-game.herokuapp.com", port: 443],
-  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
   ssl: true
 
