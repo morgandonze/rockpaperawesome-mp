d565d1334ff7d4da46e0614ce67ca59dfdb0c942
Morgan Laco <morganlaco@gmail.com>
2018-02-10 20:18:14 -0500

    Use production config from Heroku ENV variables and decrease socket timeout

--- config/prod.exs
+++ config/prod.exs
@@ -16,7 +16,15 @@ use Mix.Config
 config :rockpaperawesome, RockpaperawesomeWeb.Endpoint,
   load_from_system_env: true,
   url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
+
+config :hello, Rockpaperawesome.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: [scheme: "https", host: "rockpaperawesome-game.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
 
 # Do not print debug messages in production
 config :logger, level: :info
@@ -61,4 +69,4 @@ config :logger, level: :info
 
 # Finally import the config/prod.secret.exs
 # which should be versioned separately.
-import_config "prod.secret.exs"
+# import_config "prod.secret.exs"
--- lib/rockpaperawesome_web/channels/user_socket.ex
+++ lib/rockpaperawesome_web/channels/user_socket.ex
@@ -8,7 +8,8 @@ defmodule RockpaperawesomeWeb.UserSocket do
   channel "game:*", Rockpaperawesome.GameChannel
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket
+  transport :websocket, Phoenix.Transports.WebSocket,
+    timeout: 45_000
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
