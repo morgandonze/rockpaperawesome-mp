0f1ee1eb497ef35fe6faf3cdfa95fdf8c7400e87
Morgan Laco <morganlaco@gmail.com>
2018-02-10 20:58:49 -0500

    Try to fix DB config

--- config/prod.exs
+++ config/prod.exs
@@ -20,11 +20,11 @@ config :rockpaperawesome, RockpaperawesomeWeb.Endpoint,
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
-# config :hello, Rockpaperawesome.Repo,
-#   adapter: Ecto.Adapters.Postgres,
-#   url: System.get_env("DATABASE_URL"),
-#   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
-#   ssl: true
+config :hello, Rockpaperawesome.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
 
 # Do not print debug messages in production
 config :logger, level: :info
