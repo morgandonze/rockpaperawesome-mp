a1437c3c5fb5af2903e292e0060ca0dd93f987a4
Morgan Laco <morganlaco@gmail.com>
2018-04-08 21:24:45 -0400

    Use "x" icon for missed turn

--- assets/js/components/prompt.js
+++ assets/js/components/prompt.js
@@ -42,7 +42,7 @@ class Prompt extends Component {
   setSymbols = () => {
     let symbols = []
     symbols.push([[null], t.SHAKE_TIME])
-    symbols.push([[null, "star", "times", "exclamation-triangle"], t.RESULT])
+    symbols.push([[null, "star", "times", "times"], t.RESULT])
     symbols.push([[null], t.MOVE_TIME])
     symbols.push([["hourglass-1"], t.HURRY])
     symbols.push([["hourglass-2"], t.HURRY])
